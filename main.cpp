c6ff560d00bc69d33a0af9f160daac48a0014901
IgorKoga <ihenriquekogavigato@gmail.com>
2026-06-09 08:31:11 -0300

    se criar um arquivo compilador.exe, executa o lexico e sintatico junto

--- main.cpp
+++ main.cpp
@@ -13,13 +13,17 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  // Verifica se o executável foi chamado como "lexico"
+  // Verifica se o executável foi chamado como "lexico" ou "sintatico"
   string execName = argv[0];
   bool isLexicoOnly = (execName.find("lexico") != string::npos);
+  bool isSintaticoOnly = (execName.find("sintatico") != string::npos);
 
 #ifdef LEXICO_ONLY
   isLexicoOnly = true;
 #endif
+
+  // Se não for especificamente nenhum (ex: compilador.exe ou main.exe), executa ambos
+  bool runBoth = !isLexicoOnly && !isSintaticoOnly;
 
   // 1. Nome do arquivo fixo
   string folder = "codigoRust/"; // endereço padrão
@@ -46,9 +50,16 @@ int main(int argc, char *argv[]) {
   Scanner lexScanner(code);
 
   try {
-    if (isLexicoOnly) {
+    if (isLexicoOnly || runBoth) {
+      if (runBoth) {
+        cout << "\n============================================================" << endl;
+        cout << "                  1. FASE LEXICA (TOKENS)                   " << endl;
+        cout << "============================================================" << endl;
+      } else {
+        cout << endl;
+      }
+      
       // Cabeçalho da tabela
-      cout << endl;
       cout << string(60, '-') << endl;
       cout << left << setw(20) << "Token" << setw(30) << "Lexema" << "Linha"
            << endl;
@@ -70,13 +81,24 @@ int main(int argc, char *argv[]) {
     }
 
 #ifndef LEXICO_ONLY
-    if (!isLexicoOnly) {
+    if (isSintaticoOnly || runBoth) {
+      if (runBoth) {
+        cout << "\n============================================================" << endl;
+        cout << "                  2. FASE SINTATICA (AST JSON)              " << endl;
+        cout << "============================================================" << endl;
+      }
+
       // Cria um scanner separado para o parser
       Scanner parserScanner(code);
       Parser parser(parserScanner);
       auto ast = parser.parseProgram();
 
       cout << ast->toJson() << endl;
+
+      if (runBoth) {
+        cout << "============================================================" << endl;
+      }
+      cout << "Fim da analise sintatica." << endl;
     }
 #endif
 
